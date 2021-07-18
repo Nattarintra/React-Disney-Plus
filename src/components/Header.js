d63cc8d69a2e428d8a0748864936b3ba643fcc88
Natta <nattarintra.thutsanti@hotmail.com>
2021-07-19 01:07:29 +0200

    ADD: Actions SignIn SignOut.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,91 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import { auth, provider } from "../firebase"
 import styled from "styled-components"
 
+import { selectUserName, selectUserPhoto, setUserLogin, setSignOut } from "../features/user/userSlice"
+import { useDispatch, useSelector } from "react-redux"
+
 function Header() {
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const userName = useSelector(selectUserName)
+  const userPhoto = useSelector(selectUserPhoto)
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async user => {
+      if (user) {
+        dispatch(
+          setUserLogin({
+            name: user.displayName,
+            email: user.email,
+            photo: user.photoURL
+          })
+        )
+        history.push("/")
+      }
+    })
+  }, [])
+
+  const signIn = () => {
+    auth.signInWithPopup(provider).then(result => {
+      let user = result.user
+      dispatch(
+        setUserLogin({
+          name: user.displayName,
+          email: user.email,
+          photo: user.photoURL
+        })
+      )
+      history.push("/")
+    })
+  }
+
+  const signOut = () => {
+    auth.signOut().then(() => {
+      dispatch(setSignOut())
+      history.push("/login")
+    })
+  }
+
   return (
     <Nav>
       <Logo src="/images/logo.svg" />
-      <NavMenu>
-        <a>
-          <img src="/images/home-icon.svg" alt="home-icon" />
-          <span>Home</span>
-        </a>
-        <a>
-          <img src="/images/search-icon.svg" alt="search-icon" />
-          <span>Search</span>
-        </a>
-        <a>
-          <img src="/images/watchlist-icon.svg" alt="watchlist-icon" />
-          <span>watchList</span>
-        </a>
-        <a>
-          <img src="/images/original-icon.svg" alt="watchlist-icon" />
-          <span>original</span>
-        </a>
-        <a>
-          <img src="/images/movie-icon.svg" alt="watchlist-icon" />
-          <span>movies</span>
-        </a>
-        <a>
-          <img src="/images/series-icon.svg" alt="watchlist-icon" />
-          <span>serie</span>
-        </a>
-      </NavMenu>
-      <UserImg src="/images/natta-profile.jpg" />
+      {!userName ? (
+        <LoginContainer>
+          <Login onClick={signIn}>Login </Login>
+        </LoginContainer>
+      ) : (
+        <>
+          <NavMenu>
+            <a>
+              <img src="/images/home-icon.svg" alt="home-icon" />
+              <span>Home</span>
+            </a>
+            <a>
+              <img src="/images/search-icon.svg" alt="search-icon" />
+              <span>Search</span>
+            </a>
+            <a>
+              <img src="/images/watchlist-icon.svg" alt="watchlist-icon" />
+              <span>watchList</span>
+            </a>
+            <a>
+              <img src="/images/original-icon.svg" alt="watchlist-icon" />
+              <span>original</span>
+            </a>
+            <a>
+              <img src="/images/movie-icon.svg" alt="watchlist-icon" />
+              <span>movies</span>
+            </a>
+            <a>
+              <img src="/images/series-icon.svg" alt="watchlist-icon" />
+              <span>serie</span>
+            </a>
+          </NavMenu>
+          <UserImg onClick={signOut} src="/images/natta-profile.jpg" />
+        </>
+      )}
     </Nav>
   )
 }
@@ -99,4 +153,25 @@ const UserImg = styled.img`
   height: 40px;
   border-radius: 50%;
   cursor: pointer;
+`
+const LoginContainer = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+`
+const Login = styled.div`
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  padding: 8px 16px;
+  letter-spacing: 1.5px;
+  text-transform: uppercase;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+  transition: all 250ms ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
 `
